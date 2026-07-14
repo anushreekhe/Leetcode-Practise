c9e676686289dac0096ccd5db187ced2dd49da90
Anushree Khemani <anushreekhemani22@gmail.com>
2026-07-14 17:02:06 +0530

    Time: 11 ms (63.47%), Space: 17 MB (21.3%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findLength(string& nums1, string nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(nums1[i-1]==nums2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    
+    int minDistance(string word1, string word2) {
+        return word1.size()+word2.size()-2*findLength(word1,word2);
+    }
+};
