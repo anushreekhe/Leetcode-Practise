97ddae5de16ba038d9e6c58635878a502b462a37
Anushree Khemani <anushreekhemani22@gmail.com>
2026-07-16 01:57:44 +0530

    Time: 64 ms (59.68%), Space: 14.4 MB (48.49%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
 
-    int f(int i, int prev, vector<int>& nums, vector<vector<int>>& dp){
-        if(i==nums.size()) return 0;
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        int len=0+f(i+1,prev,nums,dp);
-        if(prev==-1||nums[i]>nums[prev]){
-            len=max(len,1+f(i+1,i,nums,dp));
-        }
-        return dp[i][prev+1]= len;
-    }
+    // int f(int i, int prev, vector<int>& nums, vector<vector<int>>& dp){
+    //     if(i==nums.size()) return 0;
+    //     if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+    //     int len=0+f(i+1,prev,nums,dp);
+    //     if(prev==-1||nums[i]>nums[prev]){
+    //         len=max(len,1+f(i+1,i,nums,dp));
+    //     }
+    //     return dp[i][prev+1]= len;
+    // }
 
     int lengthOfLIS(vector<int>& nums) {
         
-        vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1,-1));
-        return f(0,-1,nums,dp);
+        // vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1,-1));
+        // return f(0,-1,nums,dp);
+
+        int maxi=1;
+        vector<int> dp(nums.size(),1);
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=max(dp[i], 1+dp[j]);
+                }
+            }
+            maxi=max(maxi,dp[i]);
+        }
+        return maxi;
     }
 };
