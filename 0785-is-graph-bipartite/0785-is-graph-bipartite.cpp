01d0a7b0bda7ac325e2310a6ae33bfac591d24d1
Anushree Khemani <anushreekhemani22@gmail.com>
2026-08-02 02:32:38 +0530

    Time: 0 ms (100%), Space: 17.2 MB (93.15%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
 
-    bool f(int i, int V, vector<vector<int>>& adj, int color[]){
-        queue<int> q;
-        q.push(i);
-        color[i]=0;
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(auto it : adj[node]){
-                if(color[it]==-1){
-                    color[it]=!color[node];
-                    q.push(it);
-                }
-                else if(color[it]==color[node]) return false;
-            }
+    // bool f(int i, int V, vector<vector<int>>& adj, int color[]){
+    //     queue<int> q;
+    //     q.push(i);
+    //     color[i]=0;
+    //     while(!q.empty()){
+    //         int node=q.front();
+    //         q.pop();
+    //         for(auto it : adj[node]){
+    //             if(color[it]==-1){
+    //                 color[it]=!color[node];
+    //                 q.push(it);
+    //             }
+    //             else if(color[it]==color[node]) return false;
+    //         }
             
+    //     }
+    //     return true;
+    // }
+
+
+
+    bool f(int i, int col, vector<vector<int>>& adj, int color[]){
+        color[i]=col;
+        for(auto it : adj[i]){
+            if(color[it]==-1){
+                if(!f(it,!col,adj,color)) return false;
+            }
+            else if(color[it]==col){
+                return false;
+            }
         }
         return true;
+
     }
+
+
 
     bool isBipartite(vector<vector<int>>& graph) {
          int n = graph.size();
@@ -27,7 +45,7 @@ public:
     for(int i=0;i<n;i++) color[i]=-1;
     for(int i=0;i<n;i++){
         if(color[i]==-1){
-            if(f(i,n,graph,color)==false) return false;   // graph directly pass karo
+            if(f(i,0,graph,color)==false) return false;   // graph directly pass karo
         }
     }
     return true;
